bf2c8c5d2940c8055fc67fb576de80978ad55b23
juniorgamma <juniorgamma@gmail.com>
2025-04-16 08:54:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // PARTE 1: Lista de perguntas e respostas
 perguntas = [
     {
-      "pergunta": "Qual é o principal pokémon do Ash?",
+      "pergunta": "Qual é o principal POKE do Ash?",
       "respostas": [
         { "opcao": "Pikachu", "correto": true },
         { "opcao": "Bubasauro", "correto": false },
